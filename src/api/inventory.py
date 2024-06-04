65322f6e5ec10e98121bf77bef149087a6ade5b0
Kieran McNulty <kmcnulty@Kierans-Laptop.lan>
2024-06-04 00:34:22 -0700

    Trade finished and tested with manual test results

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -132,8 +132,7 @@ def trade(char_id_1: int, char_id_2: int, trade_offer: TradeOffer):
     success = True
     with db.engine.begin() as connection:
         status_string_1 = ""
-        status_string_1 = ""
-        success = False
+        status_string_2 = ""
         character_name_1 = connection.execute(sqlalchemy.text(
                 """
                 SELECT name
@@ -153,7 +152,7 @@ def trade(char_id_1: int, char_id_2: int, trade_offer: TradeOffer):
         #check trade valid
         for char_id, items, char_flag in [(char_id_1, trade_offer.offer, 1), (char_id_2, trade_offer.request, 2)]: 
             for item in items:
-                item_id, quantity = item["item_id"], item["quantity"]
+                item_id, quantity = item.item_id, item.quantity
                 item_response = connection.execute(
                     sqlalchemy.text("""
                         SELECT quantity, equipped
@@ -172,58 +171,94 @@ def trade(char_id_1: int, char_id_2: int, trade_offer: TradeOffer):
                             status_string_1 += status_string
                             status_string_2 += status_string
                         success = False
-                
-        #on trade success
-        for offered, requested in zip(trade_offer.offer, trade_offer.request):
+        if success: 
+            #on trade success
+            for offered, requested in zip(trade_offer.offer, trade_offer.request):
 
-            #modify char 1 inventory
-            #subtract offered
-            connection.execute(sqlalchemy.text("""
-                UPDATE inventory
-                SET quantity = quantity - :quantity
-                WHERE char_id = :char_id AND item_id = :item_id
-            """), {"quantity": offered["quantity"], "char_id": char_id_1, "offered_item_id": offered["item_id"]})
-            #add requested
-            connection.execute(sqlalchemy.text("""
-                UPDATE inventory
-                SET quantity = quantity + :quantity
-                WHERE char_id = :char_id AND item_id = :item_id
-            """), {"quantity": requested["quantity"], "char_id": char_id_1, "item_id": requested["item_id"]})
-            status_string_1 += character_name_1.name + " traded " + str(offered["quantity"]) + " of "+ str(offered["item_id"]) +" to " + character_name_2.name + " for " + str(requested["quantity"]) + " of " + str(requested["item_id"])
-            
+                #modify char 1 inventory
+                #subtract offered
+                connection.execute(sqlalchemy.text("""
+                    UPDATE inventory
+                    SET quantity = quantity - :quantity
+                    WHERE char_id = :char_id AND item_id = :item_id
+                """), {"quantity": offered.quantity, "char_id": char_id_1, "item_id": offered.item_id})
+                #add requested
+                existing_item = connection.execute(
+                    sqlalchemy.text("""
+                        SELECT * FROM inventory
+                        WHERE char_id = :char_id AND item_id = :item_id
+                    """),
+                    {"char_id": char_id_1, "item_id": requested.item_id}
+                ).fetchone()
+                if existing_item:  
+                    #modify
+                    connection.execute(sqlalchemy.text("""
+                        UPDATE inventory
+                        SET quantity = quantity + :quantity
+                        WHERE char_id = :char_id AND item_id = :item_id
+                    """), {"quantity": requested.quantity, "char_id": char_id_1, "item_id": requested.item_id})
+                else:  
+                    #insert
+                    connection.execute(sqlalchemy.text("""
+                        INSERT INTO inventory (char_id, item_id, quantity, equipped)
+                        VALUES (:char_id, :item_id, :quantity, false) 
+                    """), {"char_id": char_id_1, "item_id": requested.item_id, "quantity": requested.quantity})
 
-            #modify char 2 inventory
-            #subtract requested
-            connection.execute(sqlalchemy.text("""
-                UPDATE inventory
-                SET quantity = quantity - :quantity
-                WHERE char_id = :char_id AND item_id = :item_id
-            """), {"quantity": requested["quantity"], "char_id": char_id_2, "offered_item_id": requested["item_id"]})
-            #add offered
-            connection.execute(sqlalchemy.text("""
-                UPDATE inventory
-                SET quantity = quantity + :quantity
-                WHERE char_id = :char_id AND item_id = :item_id
-            """), {"quantity": offered["quantity"], "char_id": char_id_2, "item_id": offered["item_id"]})
-            status_string_2 += character_name_2.name + " traded " + str(requested["quantity"]) + " of "+ str(requested["item_id"]) +" to " + character_name_1.name + " for " + str(offered["quantity"]) + " of " + str(offered["item_id"])
+                status_string_1 += character_name_1.name + " traded " + str(offered.quantity) + " of "+ str(offered.item_id) +" to " + character_name_2.name + " for " + str(requested.quantity) + " of " + str(requested.item_id)
+               
+
+                #modify char 2 inventory
+                #subtract requested
+                connection.execute(sqlalchemy.text("""
+                    UPDATE inventory
+                    SET quantity = quantity - :quantity
+                    WHERE char_id = :char_id AND item_id = :item_id
+                """), {"quantity": requested.quantity, "char_id": char_id_2, "item_id": requested.item_id})
+                #add offered
+                existing_item = connection.execute(
+                    sqlalchemy.text("""
+                        SELECT * FROM inventory
+                        WHERE char_id = :char_id AND item_id = :item_id
+                    """),
+                    {"char_id": char_id_2, "item_id": offered.item_id}
+                ).fetchone()
+
+                if existing_item:  
+                    connection.execute(sqlalchemy.text("""
+                        UPDATE inventory
+                        SET quantity = quantity + :quantity
+                        WHERE char_id = :char_id AND item_id = :item_id
+                    """), {"quantity": offered.quantity, "char_id": char_id_2, "item_id": offered.item_id})
+                else:  
+                    connection.execute(sqlalchemy.text("""
+                        INSERT INTO inventory (char_id, item_id, quantity, equipped)
+                        VALUES (:char_id, :item_id, :quantity, false) 
+                    """), {"char_id": char_id_2, "item_id": offered.item_id, "quantity": offered.quantity})
+                status_string_2 += character_name_2.name + " traded " + str(requested.quantity) + " of "+ str(requested.item_id) +" to " + character_name_1.name + " for " + str(offered.quantity) + " of " + str(offered.item_id)
+                #now remove items from inventory with 0 quantity
+                connection.execute(
+                    sqlalchemy.text("""
+                        DELETE FROM inventory
+                        WHERE quantity = 0
+                    """)
+                )
+
+        #add trade to both characters game logs
+        if status_string_1 and status_string_2:
+            connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO game_log (description, char_id) 
+                VALUES (:description, :char_id)
+                """
+            ), { "description": status_string_1, "char_id": char_id_1})
+            connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO game_log (description, char_id) 
+                VALUES (:description, :char_id)
+                """
+            ), { "description": status_string_2, "char_id": char_id_2})
             
-            #add trade to both characters game logs
-            print(status_string)
-            if status_string_1 and status_string_2:
-                connection.execute(sqlalchemy.text(
-                    """
-                    INSERT INTO game_log (description, char_id) 
-                    VALUES (:description, :char_id)
-                    """
-                ), { "description": status_string_1, "char_id": char_id_1})
-                connection.execute(sqlalchemy.text(
-                    """
-                    INSERT INTO game_log (description, char_id) 
-                    VALUES (:description, :char_id)
-                    """
-                ), { "description": status_string_2, "char_id": char_id_2})
-                
-                return {"success": success}
-            else:
-                 return{"success": False}
+            return {"success": success}
+        else:
+                return{"success": False}
             
