53b0f4f0a539404b5e24f747831072c0a57c8f7a
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-06-05 11:53:04 -0700

    added reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -15,6 +15,16 @@ def reset():
     """
     TODO: Reset
     """
-
+    quest_ledger_reset_sql = "TRUNCATE quest_ledger RESTART IDENTITY CASCADE"
+    gold_ledger_reset_sql = "TRUNCATE gold_ledger RESTART IDENTITY CASCADE"
+    stock_reset_sql = "TRUNCATE stock RESTART IDENTITY CASCADE"
+    game_log_reset_sql = "TRUNCATE game_log RESTART IDENTITY CASCADE"
+    characters_reset_sql = "TRUNCATE characters RESTART IDENTITY CASCADE"
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(quest_ledger_reset_sql))
+        connection.execute(sqlalchemy.text(gold_ledger_reset_sql))
+        connection.execute(sqlalchemy.text(stock_reset_sql))
+        connection.execute(sqlalchemy.text(game_log_reset_sql))
+        connection.execute(sqlalchemy.text(characters_reset_sql))
     return {"success": True}
 
